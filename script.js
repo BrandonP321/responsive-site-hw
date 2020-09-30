075cfe1134947858a12f9468e4a9f6ab26b1037a
Brandon Phillips <brandon.phillips976@gmail.com>
2020-09-29 22:33:26 -0700

    updated portfolio page

---
+++ script.js
@@ -0,0 +1,31 @@
+$('.img-thumbnail').on('click', function() {
+    if ($(this).attr('alt') != 'cute kitten') {
+        $('.modal').css('display', 'block')
+        $('.modal-content').empty()
+
+        var gitUrl = $(this).attr('data-giturl')
+        var siteLink = $(this).attr('data-link')
+
+        var imgEle = $('<img>')
+        imgEle.attr('src', $(this).attr('src'))
+        imgEle.attr('class', 'lightboxImg')
+        
+        var gitEle = $('<a>')
+        gitEle.attr('href', gitUrl)
+        gitEle.attr('target', '_blank')
+        gitEle.text('View the git repository here!')
+
+        var linkEle = $('<a>')
+        linkEle.attr('href', siteLink)
+        linkEle.attr('target', '_blank')
+        linkEle.text('Check out the site here!')
+
+        $('.modal-content').append(imgEle)
+        $('.modal-content').append(linkEle)
+        $('.modal-content').append(gitEle)
+    }
+})
+
+$('.closeModal').on('click', function() {
+    $('.modal').css('display', 'none')
+})
